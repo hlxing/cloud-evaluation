6753306a7b12ac689bb78568ecf7e182844232f7
1986094900 <1986094900@qq.com>
2019-04-29 16:49:58 +0800

    class

--- src/main/java/com/hlx/cloudevaluation/service/impl/ClassServiceImpl.java
+++ src/main/java/com/hlx/cloudevaluation/service/impl/ClassServiceImpl.java
@@ -2,7 +2,9 @@ package com.hlx.cloudevaluation.service.impl;
 
 import com.hlx.cloudevaluation.mapper.SysClassMapper;
 import com.hlx.cloudevaluation.model.dto.ClassAddDTO;
+import com.hlx.cloudevaluation.model.dto.ClassUpdateDTO;
 import com.hlx.cloudevaluation.model.po.SysClass;
+import com.hlx.cloudevaluation.model.po.SysClassExample;
 import com.hlx.cloudevaluation.service.ClassService;
 import com.hlx.cloudevaluation.util.GetRandomToken;
 import org.modelmapper.ModelMapper;
@@ -22,6 +24,26 @@ public class ClassServiceImpl implements ClassService {
     public ClassServiceImpl(SysClassMapper sysClassMapper, ModelMapper modelMapper) {
         this.sysClassMapper = sysClassMapper;
         this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public void update(ClassUpdateDTO classUpdateDTO, Integer userId) {
+        SysClass sysClass = modelMapper.map(classUpdateDTO, SysClass.class);
+        sysClass.setClassUpdateAt(new Date());
+        sysClassMapper.updateByPrimaryKeySelective(sysClass);
+    }
+
+    @Override
+    public void delete(Integer classId, Integer userId) {
+        SysClassExample example = new SysClassExample();
+        SysClassExample.Criteria criteria = example.createCriteria();
+        criteria.andClassIdEqualTo(classId);
+        SysClass sysClass = new SysClass();
+        sysClass.setClassExist(false);
+        sysClass.setClassUpdateAt(new Date());
+        sysClass.setClassDeleteAt(new Date());
+
+        sysClassMapper.updateByExampleSelective(sysClass, example);
     }
 
     @Override
