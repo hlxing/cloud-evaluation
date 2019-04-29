a47c6320c0b91a8963654b8589a80a86966b46ec
hlx <603773962@qq.com>
2019-04-29 17:15:31 +0800

    班级授权DTO

---
+++ src/main/java/com/hlx/cloudevaluation/model/dto/ClassAuthDTO.java
@@ -0,0 +1,25 @@
+package com.hlx.cloudevaluation.model.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @description: 班级授权传输对象
+ * @author: hlx 2019-04-29
+ **/
+@Data
+public class ClassAuthDTO {
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @ApiModelProperty(required = true, notes = "授权Token", example = "1111")
+    private String token;
+
+    @NotNull
+    @ApiModelProperty(required = true, notes = "授权代码", example = "0为学生,1为助教")
+    private Integer authCode;
+
+}
