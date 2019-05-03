5bba3679858be400e4382939520ade889b1ee984
hlx <603773962@qq.com>
2019-05-03 19:16:57 +0800

    技能添加 DTO 修改

--- src/main/java/com/hlx/cloudevaluation/model/dto/SkillAddDTO.java
+++ src/main/java/com/hlx/cloudevaluation/model/dto/SkillAddDTO.java
@@ -15,7 +15,17 @@ public class SkillAddDTO {
 
     @NotNull
     @Size(min = 1, max = 20)
-    @ApiModelProperty(required = true, notes = "技能名称,最长20", example = "答辩")
+    @ApiModelProperty(required = true, notes = "技能名称,最长20", example = "工程能力-1")
     private String skillName;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @ApiModelProperty(required = true, notes = "技能类型,最长50", example = "工程能力")
+    private String skillType;
+
+    @NotNull
+    @Size(min = 1, max = 10)
+    @ApiModelProperty(required = true, notes = "技能代号", example = "A")
+    private String skillShortName;
 
 }
