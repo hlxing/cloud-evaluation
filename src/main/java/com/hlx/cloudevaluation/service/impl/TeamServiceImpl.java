e4e06ca8673ca7d8b01bb0e8172c1e3007a7ee0a
hlx <603773962@qq.com>
2019-05-24 00:12:59 +0800

    团队详情BUG修复

--- src/main/java/com/hlx/cloudevaluation/service/impl/TeamServiceImpl.java
+++ src/main/java/com/hlx/cloudevaluation/service/impl/TeamServiceImpl.java
@@ -79,11 +79,12 @@ public class TeamServiceImpl implements TeamService {
         TeamUserExample teamUserExample = new TeamUserExample();
         TeamUserExample.Criteria teamUserCriteria = teamUserExample.createCriteria();
         teamUserCriteria.andUserIdEqualTo(userId);
-        Integer teamId = teamUserMapper.selectByExample(teamUserExample).get(0).getTeamId();
-
-        if (teamId == null) {
-            throw new ApiException(TeamErrorEnum.TEAM_NOT_EXIST);
+        List<TeamUser> temp = teamUserMapper.selectByExample(teamUserExample);
+        if (temp == null || temp.size() == 0) {
+            return new TeamDetailVO();
         }
+        Integer teamId = temp.get(0).getTeamId();
+
 
         // 获取团队信息
         SysTeam sysTeam = sysTeamMapper.selectByPrimaryKey(teamId);
