d7da59a1d1a652d3051334c1c81ea9de8f23cf0f
1986094900 <1986094900@qq.com>
2019-05-24 21:23:56 +0800

    ..

--- src/main/java/com/hlx/cloudevaluation/service/impl/TeamServiceImpl.java
+++ src/main/java/com/hlx/cloudevaluation/service/impl/TeamServiceImpl.java
@@ -76,13 +76,14 @@ public class TeamServiceImpl implements TeamService {
             sysTeam.setTeamClass(classUserList.get(0).getClassId());
             sysTeam.setTeamCaptain(userId);
         }
-        sysTeam.setTeamExit(true);
+        sysTeam.setTeamEdit(true);
         sysTeamMapper.insert(sysTeam);
 
         //队长创建团队后默认加入团队
         TeamUser teamUser = new TeamUser();
         teamUser.setUserId(userId);
         teamUser.setTeamId(sysTeam.getTeamId());
+        teamUserMapper.insert(teamUser);
 
         return token;
     }
