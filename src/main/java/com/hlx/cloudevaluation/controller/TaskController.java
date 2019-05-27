7ecfe539f7a5e21a70e90089dce0e7dd7e4a8bd9
hlx <603773962@qq.com>
2019-05-27 15:39:48 +0800

    作业修改控制层

--- src/main/java/com/hlx/cloudevaluation/controller/TaskController.java
+++ src/main/java/com/hlx/cloudevaluation/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.hlx.cloudevaluation.controller;
 
 import com.hlx.cloudevaluation.model.dto.TaskAddDTO;
 import com.hlx.cloudevaluation.model.dto.TaskEvaluateDTO;
+import com.hlx.cloudevaluation.model.dto.TaskUpdateDTO;
 import com.hlx.cloudevaluation.model.po.ApiResult;
 import com.hlx.cloudevaluation.model.vo.TaskScoreVO;
 import com.hlx.cloudevaluation.model.vo.TaskSearchVO;
@@ -69,10 +70,17 @@ public class TaskController {
         return new ApiResult<>("evaluate success");
     }
 
-    @ApiOperation(value = "作业修改")
+    @ApiOperation(value = "作业删除")
     @GetMapping("/delete")
     public ApiResult<String> delete(@RequestParam("taskId") Integer taskId) {
         taskService.delete(taskId);
+        return new ApiResult<>("delete task success");
+    }
+
+    @ApiOperation(value = "作业修改")
+    @GetMapping("/update")
+    public ApiResult<String> update(@Valid @RequestBody TaskUpdateDTO taskUpdateDTO) {
+        taskService.update(taskUpdateDTO);
         return new ApiResult<>("delete task success");
     }
 
